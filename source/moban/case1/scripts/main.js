a2d8c45ff68b1d0187dc48a6fb35b29e4eec6614
chendong.xie <chendong.xie@innocellence.com>
2016-12-22 17:17:45 +0800

    initial

---
+++ source/moban/case1/scripts/main.js
@@ -0,0 +1,52 @@
+$(function () {
+    $('.slider').MainSlider({
+        content: [
+            {
+                imageUrl: 'assets/topbag.jpg',
+                imageTitle: '生如夏花'
+            },
+            {
+                imageUrl: 'assets/topbag.jpg',
+                imageTitle: '生如夏花'
+            },
+        ],
+        isSlider:true
+    });
+});
+
+$(function ($) {
+    $.fn.extend({
+        MainSlider: function MainSlider(options) {
+            var that = this;
+            var defaults = {
+                isSlider: false,
+
+            };
+            options = $.extend({}, defaults, options);
+            var methods = {
+                render: function render() {
+                    var content = options.content;
+                    $.each(content, function (i) {
+                        var imageUrl = content[i].imageUrl;
+                        var imageTitle = content[i].imageTitle;
+                        var imageHtml = '';
+                        if (i == 0) {
+                            imageHtml = '<div class="slider-img" id = "slier' + i +'"><img src="' + imageUrl + '" alt="' + imageTitle + '"> </div>';
+                        } else {
+                            imageHtml = '<div class="slider-img" id = "slier' + i +'" style="display:none"><img src="' + imageUrl + '" alt="' + imageTitle + '"> </div>';
+                        }
+                        $(that).append(imageHtml);
+                    })
+                }
+            };
+
+            (function init() { 
+                methods.render();
+              
+            })();
+        }
+    });
+
+
+
+} (jQuery));
