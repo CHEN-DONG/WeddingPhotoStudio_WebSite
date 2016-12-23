66fc8de809fcc2d1a8089a87fde1008ad7891470
chendong.xie <chendong.xie@innocellence.com>
2016-12-23 12:31:54 +0800

    update slider

--- source/moban/case1/scripts/main.js
+++ source/moban/case1/scripts/main.js
@@ -6,8 +6,8 @@ $(function () {
                 imageTitle: '生如夏花'
             },
             {
-                imageUrl: 'assets/topbag.jpg',
-                imageTitle: '生如夏花'
+                imageUrl: 'assets/topbag1.jpg',
+                imageTitle: '法式香颂'
             },
         ],
         isSlider: true
@@ -20,11 +20,12 @@ $(function ($) {
             var that = this;
             var defaults = {
                 isSlider: false,
-
+                leftbtn: 'slider_left',
+                rightbtn: 'slider_right'
             };
             options = $.extend({}, defaults, options);
             var methods = {
-                render: function render() {
+                render: function () {
                     var content = options.content;
                     $.each(content, function (i) {
                         var imageUrl = content[i].imageUrl;
@@ -38,14 +39,27 @@ $(function ($) {
                         }
                         $(that).append(imageHtml);
                     })
-                    btnsHtml = '<div class="slider-btns"><div class="slider-btn"><img src="assets/ar-left.png"></div><div class="slider-btn"><img src="assets/ar-right.png"></div></div>';
+                    btnsHtml = '<div class="slider-btns"><div class="slider-btn" id="' + options.leftbtn + '"><img src="assets/ar-left.png"></div><div class="slider-btn" id="' + options.rightbtn + '"><img src="assets/ar-right.png"></div></div>';
                     $(that).append(btnsHtml);
+                },
+                bindSlideBtns: function () {
+                    $(that).find('#' + options.leftbtn).bind('click', this.sliderLeft);
+                    $(that).find('#' + options.rightbtn).bind('click', this.sliderRight);
+                },
+                sliderLeft:function(){
+                   var imgs = $('.slider-img')
+                },
+                sliderRight:function(){
+                  
+                    $('#slier1').hide();
+       
+                    $('#slier0').show();
                 }
             };
 
             (function init() {
                 methods.render();
-
+                methods.bindSlideBtns();
             })();
         }
     });
